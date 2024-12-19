dbca38a2f85c6e107e5546a62eb3e8726eeb463d
MD ALIF BABU <alif@k53tech.com>
2024-12-19 07:22:35 +0600

    initial commit

---
+++ src/test/java/com/example/springloginsecurity/SpringLoginSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springloginsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
